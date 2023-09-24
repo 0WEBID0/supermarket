e32bba87cc6167ca83d1b3bf1e6211e8b1291cf1
Pecoden <90499770+0WEBID0@users.noreply.github.com>
2023-09-24 21:54:59 +0300

    add .

---
+++ scripts/form-b24d-carousel_e2c3.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => false,
+    'email' => array(
+    'from' => '',
+    'to' => '',
+    'toCopy' => '',
+    'toHiddenCopy' => ''
+    ),
+    'fields' => array(
+    'name-1' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'name-1',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'name-1\' is required.'
+    )
+    ),
+    'Password' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'Password',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Password\' is required.'
+    )
+    ),
+    'Checkbox' => array(
+    'order' => 3,
+    'type' => 'checkbox',
+    'label' => 'Checkbox',
+    'required' => false,
+    'errors' => array(
+    'required' => 'Field \'Checkbox\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
